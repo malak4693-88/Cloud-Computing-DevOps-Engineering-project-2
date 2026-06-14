0907ab9f08f00d7ea6f2d7a860f3faf3755b1b45
Malak Abdalrahim <malek_4693@limu.edu.ly>
2026-06-14 19:53:32 +0200

    Configure Terraform remote backend in Azure Storage

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "malak-mayar-tfstate-rg"
+    storage_account_name = "malakmayartfstate4693"
+    container_name       = "tfstate"
+    key                  = "cloudscale-project2.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
